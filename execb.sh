e3a756d4445d0722837c235b20efd08437d68805
Braully Silva <braully@gmail.com>
2025-11-24 14:47:36 -0300

    ajuste parametros de execução

--- execb.sh
+++ execb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx2g -cp target/hnv-code-data-article-1.0.0.jar io.github.braully.graph.exec.ExecBigDataSets
+java -Xmx5g -cp target/hnv-code-data-article-1.0.0.jar io.github.braully.graph.exec.ExecBigDatasetParallel
